55a18406af2a0f5490fe4c7bd8867c0cf66a3ed2
junhyeokahn <junhyeokahn91@gmail.com>
2017-06-14 20:38:33 -0500

    policy grad

---
+++ DQN/BreakoutEnv.py
@@ -0,0 +1,24 @@
+import ipdb
+import gym
+import numpy as np
+from matplotlib import pyplot as plt
+
+# ipdb.sset_trace()
+env = gym.envs.make("Breakout-v0")
+
+print("Action space size: {}".format(env.action_space.n))
+print(env.get_action_meanings())
+
+observation = env.reset()
+print("Observation space shape: {}".format(observation.shape))
+
+plt.figure()
+plt.imshow(env.render(mode='rgb_array'))
+[env.step(2) for x in range(1)]
+plt.figure()
+plt.imshow(env.render(mode='rgb_array'))
+
+env.render(close=True)
+
+# Check out what a cropped image looks like
+plt.imshow(observation[34:-16,:,:])
